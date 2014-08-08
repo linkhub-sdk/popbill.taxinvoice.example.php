05aa8453a7ab10025963ada5d2f4cba5d97fbed5
김성준 <kimsj@innopost.com>
2014-08-08 11:25:13 +0900

    Update Submodule, Add GetEPrintURL.

---
+++ GetEPrintURL.php
@@ -0,0 +1,16 @@
+<?php
+
+include 'common.php';
+
+
+echo '세금계산서 공급받는자용 인쇄화면 URL 확인 테스트'.chr(10).'처리결과 : ' ;
+
+try {
+	$result = $TaxinvoiceService->GetEPrintURL('1231212312',ENumMgtKeyType::SELL,'123123','userid');
+	echo substr( $result,0,40) . ' ...생략...';
+}
+catch(PopbillException $pe) {
+	echo '['.$pe->getCode().'] '.$pe->getMessage();
+}
+echo chr(10);
+?>
